dc1fc98871cc2fe074fa943dbb1787f5b6051016
Ayush7809 <104785802+Ayush7809@users.noreply.github.com>
2022-08-22 22:35:08 +0530

    Add files via upload

---
+++ greater no.py
@@ -0,0 +1,8 @@
+a=int(input('Enter first no'))
+b=int(input('Enter second no'))
+if a>b:
+    print("a is greater than b")
+elif b>a:
+    print("b is greater than a")
+else:
+    print("a and b are equal")
